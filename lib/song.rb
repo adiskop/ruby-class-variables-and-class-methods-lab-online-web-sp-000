773152776f2652aa0e00c78a8b62b81bfdd47581
Adi Skop <adiskop@gmail.com>
2020-07-14 14:31:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,17 +28,16 @@ def self.genres
 end
 
 def self.genre_count
-  genre_count={}
-  @@genres.each do |genre|
+   genre_count = {}
+   @@genres.each do |genre|
      if genre_count[genre]
-       genre_count[genre] += 1
+       genre_count[genre] += 1 
      else
        genre_count[genre] = 1
      end
    end
    genre_count
  end
-
 end
 
 
